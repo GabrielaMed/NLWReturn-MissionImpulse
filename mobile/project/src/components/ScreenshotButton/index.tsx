964c46c62c271089e96c93323372c1d1b98e8dfa
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2022-05-16 09:12:20 -0400

    mobile

---
+++ mobile/project/src/components/ScreenshotButton/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, TouchableOpacity, Image } from 'react-native';
+
+import { styles } from './styles';
+
+export function ScreenshotButton({screenshot, onTakeShot, onRemoveShot}) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      
+    </TouchableOpacity>
+  );
+}
